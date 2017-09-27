7f52818d69eca2237f7eaa7ca490d10a433c2550
N3\dmhabibulin <dhabibullin@freshdoc.ru>
2017-09-27 17:06:07 +0500

    убрал лишние директивы

--- RegistrationUsePageObject/Tests/LoginTests.cs
+++ RegistrationUsePageObject/Tests/LoginTests.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿
 using RegistrationUsePageObject.TestHelper;
 using NUnit.Framework;
-using TestingProject.Tests;
 using RegistrationUsePageObject.Framework;
 
 namespace TestingProject.Tests
